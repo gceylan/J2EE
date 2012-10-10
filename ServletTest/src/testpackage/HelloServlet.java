32b792b75c49683f51788dfd1558d8d55b5c9b80
gceylan <gokhan.ceylan@bil.omu.edu.tr>
2012-10-10 16:06:37 +0300

    servlet-test

---
+++ ServletTest/src/testpackage/HelloServlet.java
@@ -0,0 +1,52 @@
+package testpackage;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet(description = "Merhaba Servlet", urlPatterns = { "/hello" })
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		
+		PrintWriter out = response.getWriter();
+		out.println(
+				"<! DOCTYPE html>\n" +
+				"<html>\n" +
+				"<head><title>A Test Servlet</title></head>\n" +
+				"<body>\n" +
+				"<h1>Test</h1>\n" +
+				"<p>Hello Servlet!</p>\n" +
+				"</body></html>"
+		);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
