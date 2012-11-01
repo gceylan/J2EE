d080d72bfa09c8039daae3e9255425565d93c304
gceylan <gokhan.ceylan@bil.omu.edu.tr>
2012-11-01 23:01:43 +0200

    servlet uygulamaları

---
+++ ServletApp/src/coreservlet/FormServlet.java
@@ -0,0 +1,83 @@
+package coreservlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FormServlet
+ */
+public class FormServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FormServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		response.setContentType("text/html");
+		
+		out.println(ServletUtilities.getHeadWithCss("Servlet Result"));
+		
+		out.println(ServletUtilities.openMainDiv("Servlet Form Result"));
+		
+		out.println("<table class=\"table table-hover\">");
+		out.println("<tr><th>#</th><th>Parameter Name</th><th>Parameter Value</th></tr>");
+		
+		Enumeration<String> parameterNames = request.getParameterNames();
+		int i = 0;
+		
+		while(parameterNames.hasMoreElements()) {
+			String parameterName = parameterNames.nextElement();
+			
+			String[] parameterValues = request.getParameterValues(parameterName);
+			
+			out.println("<tr>");
+			out.print("<td>" + ++i + "</td>");
+			
+			if (parameterValues.length == 1) {
+				String parameterValue = parameterValues[0];
+				
+				if (parameterValue.trim().equals("")) {
+					out.println("<td>" + parameterName + "</td><td><i>No Value</i></td>");
+				} else {
+					out.println("<td>" + parameterName + "</td><td>" + parameterValue + "</td>");
+				}
+			} else {
+				for (String value : parameterValues) {
+					out.println("<li>" + value + "</li>");
+				}
+			}
+			
+			out.println("</tr>");
+			
+		}
+		
+		out.println("</table>");
+		
+		out.println(ServletUtilities.closeMainDiv());
+		
+		out.println(ServletUtilities.getFooter("./form.html"));
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
---
+++ ServletApp/src/coreservlet/ServletUtilities.java
@@ -0,0 +1,42 @@
+package coreservlet;
+
+public class ServletUtilities {
+	
+	public static String getHeadWithCss(String title) {
+		StringBuffer sb = new StringBuffer();
+		sb.append("<!DOCTYPE html>\n");
+		sb.append("<html>\n").append("<head>\n");
+		sb.append("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n");
+		sb.append("<link href=\"assets/css/bootstrap.css\" rel=\"stylesheet\">\n");
+		sb.append("<link href=\"assets/css/bootstrap-responsive.css\" rel=\"stylesheet\">\n");
+		sb.append("<link href=\"assets/css/style.css\" rel=\"stylesheet\">\n");
+		sb.append("<title>").append(title).append("</title>\n");
+		sb.append("</head>\n");
+		
+		return sb.toString();
+	}
+	
+	public static String openMainDiv(String mainText) {
+		return "<div class=\"container\">\n" +
+				 "<div class=\"row\">\n" +
+				   "<div class=\"span12\">\n" +
+				     "<h2>" + mainText + "</h2>\n";
+	}
+	
+	public static String closeMainDiv() {
+		return "</div>\n</div>\n</div>\n";
+	}
+	
+	public static String getFooter(String back) {
+		StringBuffer sb = new StringBuffer();
+		
+		sb.append("<footer>\n").append("<div class=\"container\">").append("<hr>\n");
+		sb.append("<p class=\"pull-right\"><a href=" + back + "><i class=\"icon-arrow-left\"></i> Back to top</a></p>\n");
+		sb.append("<ul>\n");
+		sb.append("<li><a href=\"./home.html\"><i class=\"icon-home\"></i> Home</a></li>\n");
+		sb.append("</ul>\n").append("</div>\n").append("</footer>\n");
+		
+		return sb.toString();
+	}
+
+}
